16cde593ac19dca42523bdb8bb779f52e91372f5
Opalao0km <gabriel.dias.br36@gmail.com>
2023-03-07 13:42:55 -0300

    alteracoes

---
+++ src/components/ButtonSlider/styles.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+import { colors } from '../../styles/colors';
+
+export const styles = StyleSheet.create({
+    ball: {
+        width: 30,
+        height: 30,
+        backgroundColor: colors.third,
+        borderRadius: 15
+    }
+})
+
+
+
+
+
