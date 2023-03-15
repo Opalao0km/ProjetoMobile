aab13aba5cb772f571259edfa9435a78973c603a
Opalao0km <gabriel.dias.br36@gmail.com>
2023-03-15 15:10:13 -0300

    sucesso

---
+++ src/screens/Slider1/index.tsx
@@ -0,0 +1,35 @@
+import { FlatList, ImageBackground, View } from 'react-native';
+import { IPage } from '../../../App';
+import {
+    ComponentButtonSlider, ComponentListMarker, ComponentTitleSlider
+} from '../../components';
+import { styles } from './styles';
+export function Slider1({ setPageI }: IPage) {
+    const slide1 = require("../../assets/slide1.png")
+    const slide1Texts = [
+        { id: '1', text: 'Localize seu veículo.'},
+        { id: '2', text: 'Rastreamento de movimento por gps 24 horas.'},
+        { id: '3', text: 'Acompanhe a rota do início ao fim.'},
+    ]
+    return (
+        <ImageBackground source={slide1} style={styles.container} >
+            <View style={styles.panel}>
+                <ComponentTitleSlider titleI='Sistema de rastreamneto' />
+                <FlatList
+                    data={slide1Texts}
+                    renderItem={({ item }) =>
+                        <ComponentListMarker key={item.id} textMarker={item.text} />
+                    }
+                    keyExtractor={(item) => item.id}
+                />
+            </View>
+            <View style={styles.buttonSlider}>
+                <ComponentButtonSlider onPressI={() => setPageI(1)} />
+                <ComponentButtonSlider onPressI={() => setPageI(2)} />
+                <ComponentButtonSlider onPressI={() => setPageI(3)} />
+                <ComponentButtonSlider onPressI={() => setPageI(4)} />
+            </View>
+        </ImageBackground>
+    );
+
+}
