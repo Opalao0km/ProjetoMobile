9acfc3d50c7c09b48895173b59319dcf0939d7a2
Opalao0km <gabriel.dias.br36@gmail.com>
2023-06-19 12:13:50 -0300

    sucess

--- src/screens/Location/styles.ts
+++ src/screens/Location/styles.ts
@@ -1,18 +1,33 @@
-import { Dimensions, StyleSheet } from "react-native";
-
+import {Dimensions, StyleSheet} from 'react-native'
+import { colors } from '../../styles/colors';
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      padding: 20,
-    },
-    paragraph: {
-      fontSize: 18,
-      textAlign: 'center',
-    },
-    map: {
-        width: Dimensions.get("window").width,
-        height: Dimensions.get("window").height
-    }
-  });
+   container: {
+     flex: 1,
+     alignItems: 'center',
+     justifyContent: 'center',
+     padding: 20,
+   },
+   paragraph: {
+     fontSize: 18,
+     textAlign: 'center',
+   },
+   map:{
+     widht: Dimensions.get("window").width,
+     height: Dimensions.get("window").height
+   },
+   searchContainer:{
+     position: 'absolute',
+     zIndex: 1,
+     width: '90%' ,
+     top: 10
+
+
+   },
+   searchInput:{
+     height: 56,
+     borderRadius: 7,
+     borderWidth: 2,
+     borderColor: colors.third
+   }
+ });
+ 
